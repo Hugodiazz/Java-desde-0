61599f92a8c5223352782a80126d448106986a37
Hugodiazz <hugozaid2002@gmail.com>
2024-06-05 19:39:33 -0600

    Comentarios
    Ejercicio para realizar comentarios en Java.

---
+++ Java/Introduccion/Comentarios.java
@@ -0,0 +1,14 @@
+package Introduccion;
+public class Comentarios{
+    public static void main(String[] args) {
+        //Realiza comentarios de una sola línea en Java con una doble diagonal.
+        System.out.println("Hola Mundo"); //Este es mi primer hola mundo
+
+        /*
+        También puedes realizar comentarios de multiples lineas de esta manera.
+        Esto no será leido por el compilador al momento de leer el código, por lo que
+        puedes escribir aquí lo que quieras.
+        Intenta compilar el código y ve lo que pasa
+         */
+    }
+}
